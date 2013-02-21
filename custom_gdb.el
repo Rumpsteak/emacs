5b9f7e87a5751f12e0a810ff96417a35878db00a
Mikael Lars Johansson <mikaeljo@axis.com>
2013-02-21 13:54:40 +0100

    Basic gdb setup

--- custom_gdb.el
+++ custom_gdb.el
@@ -1,14 +1,76 @@
 ;; So, all we need to do now is to split the buffers properly.
 ;; What is the difference between buffers and windows?
 
+(defun my-gdb-setup-windows ()
+  "Adapted from `gdb-setup-windows'."
+  (gdb-display-locals-buffer)
+  (gdb-display-stack-buffer)
+  (delete-other-windows)
+  (gdb-display-breakpoints-buffer)
+  (delete-other-windows)
+  ; Don't dedicate.
+  (pop-to-buffer gud-comint-buffer)
+  (split-window nil ( / ( * (window-height) 3) 4))
+  (split-window nil ( / (window-height) 3))
+  (split-window-horizontally)
+  (other-window 1)
+  (gdb-set-window-buffer (gdb-locals-buffer-name))
+  (other-window 1)
+  (switch-to-buffer
+       (if gud-last-last-frame
+           (gud-find-file (car gud-last-last-frame))
+         (if gdb-main-file
+             (gud-find-file gdb-main-file)
+           ;; Put buffer list in window if we
+           ;; can't find a source file.
+           (list-buffers-noselect))))
+  (setq gdb-source-window (selected-window))
+  ;; (when gdb-use-separate-io-buffer
+  ;;   (split-window-horizontally)
+  ;;   (other-window 1)
+  ;;   (gdb-set-window-buffer
+  ;;    (gdb-get-buffer-create 'gdb-inferior-io)))
+
+  ; We always want a separate I/O buffer
+  (split-window-horizontally)
+  (other-window 1)
+  (gdb-set-window-buffer
+   (gdb-get-buffer-create 'gdb-inferior-io))
+
+  (other-window 1)
+  (gdb-set-window-buffer (gdb-stack-buffer-name))
+  (split-window-horizontally)
+  (other-window 1)
+  (gdb-set-window-buffer (gdb-breakpoints-buffer-name))
+  (other-window 1)
+)
+
+
+; FIXME:
+;
+; Frame 1
+; === GDB == Locals =====
+; ==== Source ===========
+; ==== Stack ===========
+;
+; Frame 2
+; === Threads === Breaks ===
+; === Assembler ===
+; === Registers ===
+; === I/O ===
+
+
+
 (defun my-gdb-customization ()
   (progn
-    (gdb-display-locals-buffer)
-    (gdb-display-stack-buffer)
-    (gdb-display-threads-buffer)
-    (gdb-display-assembler-buffer)
-    (gdb-display-registers-buffer)
-    (gdb-display-separate-io-buffer)
+    ;; (gdb-display-locals-buffer)
+    ;; (gdb-display-stack-buffer)
+    ;; (gdb-display-threads-buffer)
+    ;; (gdb-display-assembler-buffer)
+    ;; (gdb-display-registers-buffer)
+    ;; (gdb-display-separate-io-buffer)
+    (setq gdb-use-separate-io-buffer t)
+    (my-gdb-setup-windows)
     (message "Customizing GDB!1111!!!!!!!!!!!")
     ))
 
